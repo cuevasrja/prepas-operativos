532db0ab29c5086223cb70d1b99d0feef13590ec
Juan Cuevas <19-10056@usb.ve>
2025-06-16 19:11:19 -0400

    Agregar script para contar la cantidad de veces que aparece una palabra en un archivo y crear archivo de texto de ejemplo

---
+++ sem8/count.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This is a simple script to count the number of times a word appears in a file.
+if [ $# -ne 2 ]; then
+    echo -e "\033[91;1mUsage:\033[0m $0 <word> <file>"
+    exit 1
+fi
+word=$1
+file=$2
+if [ ! -f "$file" ]; then
+    echo -e "\033[1;91mFile not found!\033[0m"
+    exit 1
+fi
+count=$(grep -o -i "$word" "$file" | wc -l)
+echo -e "\033[1;92mThe word '$word' appears $count times in the file '$file'.\033[0m"
